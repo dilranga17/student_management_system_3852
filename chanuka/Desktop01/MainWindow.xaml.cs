9d6d4124d2bb5d7cdd0be25af199ce3199ae2b87
Chanuka17 <117577521+Chanuka17@users.noreply.github.com>
2023-07-16 22:44:18 +0530

    Initial commit

---
+++ chanuka/Desktop01/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Desktop01
+{
+   
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataContext = new MainWindowVM();
+            InitializeComponent();
+
+        }
+
+          
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+      
+            this.Close();
+        }
+
+
+
+        private void Listview_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+     
+
+    }
+
+
+    
+}
